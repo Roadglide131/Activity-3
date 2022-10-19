ab0b6c64630e3fcd677c86704c9c9321186a4a8d
Roadglide131 <darrenj@oldschoolrenovation.com>
2022-10-18 21:22:19 -0500

    final class commit

---
+++ 6.js
@@ -0,0 +1,30 @@
+// Comparison operators - always evaluate to a boolean value
+const total = 30;
+console.log(3 < 2); // false
+console.log(total <= 3); // false
+console.log(total >= 30); // true
+
+
+// '===' is a strict equality operator
+// '===' checks for equality in value and type
+console.log(total === otherTotal); // false
+console.log("30" === 30 otherTotal); // true
+console.log("30" == 30 otherTotal); // true
+
+console.log(message);
+
+const age = 18
+
+if (age < 21) {
+  console.log("no drinks for you")
+} else {
+  console.log("PARTY")
+}
+
+//TODO:
+
+if(total > 20) {
+  console.log("You get a 10% discount");
+} else {
+  console.log("You get a 5% discount");
+}
