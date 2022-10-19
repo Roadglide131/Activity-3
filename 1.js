66ac0e07e73f06a41ac93f93fe45dfaf7472cc4c
Roadglide131 <darrenj@oldschoolrenovation.com>
2022-10-18 20:27:49 -0500

    practice

---
+++ 1.js
@@ -0,0 +1,2 @@
+const greeting2 = "Hello my name is ${Ricky} and I am ${1} year old";
+console.log(greeting);
---
+++ 3.js
@@ -0,0 +1,6 @@
+let item = "beer"
+let price = "6"
+let quantity = "1"
+let checkout =
+ let message = 'Your tab: ' ${quauntity} ${item}s for $${total}';
+ console.log(message);
